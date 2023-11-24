5f26b8ac3a53dec3e370fd0fd9f5bb1116be3c7d
danyael_vasquez <d.vsquezojeda@uandresbello.edu>
2023-11-24 15:52:08 -0300

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Subiendo Vite-React app</h1>
+      <h1>App de React Vite en vercel</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
